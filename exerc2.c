10d72a15ae0dc7211ddbcf646ea6079894c13478
Breno Rosa <71451784+Akunozz@users.noreply.github.com>
2023-03-22 18:51:31 -0300

    a

---
+++ exerc2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int negativos (int n, float *vet);
+int main () {
+    int n,i;
+    float vet;
+    printf("Digite o tamanho do vetor: ");
+    scanf("%d", &n);
+    printf("Digite os numeros reias: ");
+    vet=(float*)malloc(n*sizeof(float));
+    for (i = 0; i < n; n++)
+    {
+        /* code */
+    }
+    
+    }
